643878924fac0ab5394abd31946ccd024525072f
yashvtw <inf57201@adobe.com>
2026-02-17 17:54:13 +0530

    updated the changes for cards custom component

--- blocks/form/components/cards/cards.js
+++ blocks/form/components/cards/cards.js
@@ -1,22 +1,56 @@
 /**
- * Custom cards component
- * Based on: Radio Group
- */
+* Custom cards component
+* Based on: Radio Group
+*/
 
 /**
- * Decorates a custom form field component
- * @param {HTMLElement} fieldDiv - The DOM element containing the field wrapper. Refer to the documentation for its structure for each component.
- * @param {Object} fieldJson - The form json object for the component.
- * @param {HTMLElement} parentElement - The parent container element of the field.
- * @param {string} formId - The unique identifier of the form.
- */
-export default async function decorate(fieldDiv, fieldJson, parentElement, formId) {
-  console.log('⚙️ Decorating cards component:', fieldDiv, fieldJson, parentElement, formId);
-  
-  // TODO: Implement your custom component logic here
-  // You can access the field properties via fieldJson.properties
-  // You can access the parent container via parentElement
-  // You can access the form ID via formId
-  
-  return fieldDiv;
+* Decorates a custom form field component
+* @param {HTMLElement} fieldDiv - The DOM element containing the field wrapper. Refer to the documentation for its structure for each component.
+* @param {Object} fieldJson - The form json object for the component.
+* @param {HTMLElement} parentElement - The parent container element of the field.
+* @param {string} formId - The unique identifier of the form.
+*/
+import { createOptimizedPicture } from '../../../../scripts/aem.js';
+import { subscribe } from '../../rules/index.js';
+
+function createCard(element, enums) {
+  element.querySelectorAll('.radio-wrapper').forEach((radioWrapper, index) => {
+    if (enums[index]?.name) {
+      let label = radioWrapper.querySelector('label');
+      if (!label) {
+        label = document.createElement('label');
+        radioWrapper.appendChild(label);
+      }
+      label.textContent = enums[index]?.name;
+    }
+    radioWrapper.querySelector('input').dataset.index = index;
+    let imageUrl = enums[index].image;
+    if(imageUrl && imageUrl !== undefined) {
+      imageUrl = imageUrl.replace('hlx','aem');
+    }
+    const image = createOptimizedPicture(imageUrl || 'https://main--afb--jalagari.aem.page/lab/images/card.png', 'card-image');
+   radioWrapper.appendChild(image);
+  });
+}
+
+export default function decorate(element, fieldJson, container, formId) {
+    element.classList.add('card');
+    createCard(element, fieldJson.enum);
+    subscribe(element, formId, (fieldDiv, fieldModel) => {
+        fieldModel.subscribe((e) => {
+            const { payload } = e;
+            payload?.changes?.forEach((change) => {
+                if (change?.propertyName === 'enum') {
+                    createCard(element, change.currentValue);
+                }
+            });
+        });
+
+        element.addEventListener('change', (e) => {
+            e.stopPropagation();
+            const value = fieldModel.enum?.[parseInt(e.target.dataset.index, 10)];
+            fieldModel.value = value.name;
+        });
+    });
+    return element;
 }
