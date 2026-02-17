2552df0e2a0974873f5f46bd8be625d49949f627
yashvtw <inf57201@adobe.com>
2026-02-17 14:30:50 +0530

    Added mask mobile number function

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -581,3 +581,16 @@ export default async function decorate(block) {
     container.replaceWith(form);
   }
 }
+/**
+* Masks the first 5 digits of the mobile number with *
+* @param {*} mobileNumber
+* @returns {string} returns the mobile number with first 5 digits masked
+*/
+export function maskMobileNumber(mobileNumber) {
+if (!mobileNumber) {
+return '';
+}
+const value = mobileNumber.toString();
+// Mask first 5 digits and keep the rest
+return ` ${'*'.repeat(5)}${value.substring(5)}`;
+}
